72274aba291b238a1a905d6019136be685a27d39
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-02-13 21:35:15 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,46 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  // Array for suits
+  const SUITS = ["Hearts", "Spades", "Clubs", "Diamonds"];
+
+  // Array for card values
+  const CARD_VALUES = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King"
+  ];
+
+  // Function to generate random suit
+  const getRandomSuit = () => {
+    return SUITS[Math.floor(Math.random() * SUITS.length)];
+  };
+
+  // Function to generate random card value
+  const getRandomValue = () => {
+    return CARD_VALUES[Math.floor(Math.random() * CARD_VALUES.length)];
+  };
+
+  // Function to generate a random card
+  const generateRandomCard = () => {
+    const suit = getRandomSuit();
+    const value = getRandomValue();
+
+    return { suit, value };
+  };
+
+  // Generate and log a random card
+  const card = generateRandomCard();
+  console.log(`New random card: ${card.value} of ${card.suit}`);
 };
