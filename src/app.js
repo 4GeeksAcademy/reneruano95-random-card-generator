2cf2ec5d339cf8e2a776b073954fce330def9f0d
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-02-14 12:30:42 +0000

    another commit

--- src/app.js
+++ src/app.js
@@ -5,30 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Array for suits
+const suits = ["spade", "club", "heart", "diamond"];
+
+// Array for card values
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
-  // Array for suits
-  const suits = ["spade", "club", "heart", "diamond"];
-
-  // Array for card values
-  const values = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-
+  //
   const generateRandomSuit = () => {
     const randomSuitIndex = Math.floor(Math.random() * suits.length);
     return suits[randomSuitIndex];
@@ -40,6 +41,46 @@ window.onload = function() {
   };
 
   document.querySelector(".card").classList.add(generateRandomSuit());
-
   document.querySelector(".card").innerHTML = generateRandomNumber();
+
+  //
+  const generateRandomCard = () => {
+    const suit = generateRandomSuit();
+    const value = generateRandomNumber();
+
+    return { suit, value };
+  };
+
+  const updateCard = () => {
+    const card = generateRandomCard();
+    document.querySelector(".card").classList.remove(...suits);
+    document.querySelector(".card").classList.add(card.suit);
+    document.querySelector(".card").innerText = card.value;
+  };
+
+  let timer;
+  let timerStatus = "stopped";
+
+  // Start/stop timer function
+  const startStopTimer = () => {
+    if (timerStatus === "stopped") {
+      timer = setInterval(updateCard, 2000);
+      timerStatus = "started";
+      document.querySelector("#start-stop-timer").innerText = "Stop Timer";
+    } else {
+      clearInterval(timer);
+      timerStatus = "stopped";
+      document.querySelector("#start-stop-timer").innerText = "Start Timer";
+    }
+  };
+
+  // Generate a new card and update the webpage
+  const newCard = () => {
+    updateCard();
+  };
+
+  document.querySelector("#new-card").addEventListener("click", newCard);
+  document
+    .querySelector("#start-stop-timer")
+    .addEventListener("click", startStopTimer);
 };
